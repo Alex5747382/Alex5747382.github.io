6f368ba8ede0c5ed0815d1f3f293f7904fee948e
Zursahez <alexandrosmoschopoulos2003@gmail.com>
2025-11-20 18:22:23 +0200

    Delete JAVASCRIPT directory

--- JAVASCRIPT/demo.js
+++
@@ -1,5 +0,0 @@
-if(document.getElementById("a.linkedin") == onclick){
-   console.log("I love football");
-}else if(document.getElementById("a#j")){
-    console.log("This is my Linkedin Profile");
-}
